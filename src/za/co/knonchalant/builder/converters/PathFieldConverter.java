96dc9ee8ad156cb3032865e64769f8d9b9b3e53e
Evan Knowles <evanjamesknowles@gmail.com>
2015-07-24 18:09:11 +0200

    Initial commit.

---
+++ src/za/co/knonchalant/builder/converters/PathFieldConverter.java
@@ -0,0 +1,55 @@
+package za.co.knonchalant.builder.converters;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFieldBuilder;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Window;
+import za.co.knonchalant.builder.TaggedParameters;
+
+import java.io.File;
+
+/**
+ * Generate a Browse button if the field is not read-only.
+ */
+public class PathFieldConverter extends StandardTextFieldConverter {
+    @Override
+    public String parse(Node node, boolean readOnly) {
+        Node actualPath = node.lookup(".actual-path");
+
+        if (actualPath != null) {
+            return super.parse(actualPath, false);
+        }
+
+        return super.parse(node, true);
+    }
+
+    @Override
+    public Node convert(String object, boolean readOnly, TaggedParameters parameters, final Window window) {
+        if (readOnly) {
+            return super.convert(object, true, parameters, window);
+        }
+
+        VBox vbox = new VBox();
+        final TextField path = TextFieldBuilder.create().text(object).build();
+        path.getStyleClass().add("actual-path");
+        Button button = new Button("Browse");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                DirectoryChooser directoryChooser = new DirectoryChooser();
+                File selectedDirectory = directoryChooser.showDialog(window);
+
+                if (selectedDirectory != null) {
+                    path.setText(selectedDirectory.getAbsolutePath());
+                }
+            }
+        });
+        vbox.getChildren().addAll(path, button);
+        return vbox;
+    }
+}
---
+++ src/za/co/knonchalant/builder/converters/StandardTextFieldConverter.java
@@ -0,0 +1,47 @@
+package za.co.knonchalant.builder.converters;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextFieldBuilder;
+import javafx.scene.control.TextInputControl;
+import javafx.stage.Window;
+import za.co.knonchalant.builder.TaggedParameters;
+
+/**
+ * Standard converter for a String or String-like object.
+ */
+public class StandardTextFieldConverter extends BaseConverter<String> {
+    @Override
+    public Node convert(String object, boolean readOnly, TaggedParameters parameters, Window window) {
+        Node returned;
+        if (!readOnly) {
+            String prompt = (String) parameters.get("prompt");
+            if (prompt == null) {
+                prompt = "";
+            }
+
+            returned = TextFieldBuilder.create().text(object).promptText(prompt).build();
+            if (isTagSet()) {
+                returned.setOnKeyReleased((javafx.event.EventHandler<? super javafx.scene.input.KeyEvent>) parameters.get(getTag()));
+            }
+        } else {
+            returned = new Label(object);
+        }
+        return returned;
+    }
+
+    @Override
+    public String parse(Node node, boolean readOnly) {
+        if (TextInputControl.class.isAssignableFrom(node.getClass())) {
+            TextInputControl field = (TextInputControl) node;
+            return field.getText();
+        }
+
+        if (Label.class.isAssignableFrom(node.getClass())) {
+            Label field = (Label) node;
+            return field.getText();
+        }
+
+        return null;
+    }
+}
